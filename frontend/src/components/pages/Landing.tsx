421edbd0a2fe7fd4c46ef55542ce12544bf4a9fe
amrutsabale <samrut355@gmail.com>
2024-08-09 18:02:51 +0530

    created initial frontend

---
+++ frontend/src/components/pages/Landing.tsx
@@ -0,0 +1,16 @@
+const Landing = () => {
+  return (
+    <div className="flex">
+      <img src="chessBoard.jpeg" />
+      <div
+        className="bg-green-400 rounded-md text-white h-12 w-52 flex text-center
+      justify-center
+      items-center"
+      >
+        Play
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
